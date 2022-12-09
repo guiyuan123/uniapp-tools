5a783becb6d04e04362de3d21dd6763b1eff3544
liujiayuan.liu <liujiayuan.liu>
2022-12-09 18:52:31 +0800

    first commit

---
+++ components/Drogue/DrogueItem.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="drogue-item" :class="iconType">
+		<icon></icon>
+		<view>
+			<slot></slot>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "DrogueItem",
+		props: {
+			iconType: {
+				type: String,
+				default: 'primary' // primary、info、danger
+			}
+		}
+	}
+</script>
+
+<style>
+	.drogue-item {
+		width: 50px;
+		height: 50px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 50%;
+		font-size: 12px;
+	}
+
+	.drogue-item.primary {
+		background-color: rgb(80, 133, 212);
+		color: #fff;
+	}
+
+	.drogue-item.info {
+		background-color: #fff;
+		color: rgb(80, 133, 212);
+	}
+
+	.drogue-item.danger {
+		background-color: #fff;
+		color: rgb(214, 70, 89);
+	}
+</style>
---
+++ pages/drogue/drogue.vue
@@ -0,0 +1,67 @@
+<template>
+	<view class="container" @touchmove="handleScroll">
+		<view>
+
+		</view>
+		<view class="drogue" :class="{ 'close': isClose }">
+			<DrogueItem iconType="primary">开门</DrogueItem>
+			<DrogueItem iconType="info">续单</DrogueItem>
+			<DrogueItem iconType="danger">离店</DrogueItem>
+		</view>
+	</view>
+</template>
+
+<script>
+	import DrogueItem from '@/components/Drogue/DrogueItem.vue'
+
+	export default {
+		components: {
+			DrogueItem
+		},
+		data() {
+			return {
+				delay: 1500,
+				isClose: false,
+				timer: null
+			};
+		},
+		methods: {
+			handleScroll() {
+				this.isClose = true
+				this.destroyScroll()
+				this.timer = setTimeout(() => {
+					this.isClose = false
+				}, this.delay)
+			},
+			destroyScroll() {
+				clearTimeout(this.timer)
+				this.timer = null
+			}
+		}
+	}
+</script>
+
+<style>
+	.container {
+		height: 3000px;
+		width: 100%;
+		background-color: #9e9e9e;
+	}
+
+	.drogue {
+		position: fixed;
+		right: 0;
+		bottom: 50px;
+		padding: 20px 10px;
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		-webkit-transition: transform 0.3s;
+		transition: transform 0.3s;
+	}
+
+	.close {
+		-webkit-transform: translateX(650%);
+		transform: translateX(65%);
+	}
+</style>
